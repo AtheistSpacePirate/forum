5b349e1a64b1f0b68931b8032a97b81a7307e104
AtheistSpacePirate <mcgyvr2009i@gmail.com>
2016-04-02 17:36:54 -0400

    Added files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+  <head>
+    <title>
+      tuxforum home page
+    </title>
+    <link rel='stylesheet' type='text/css' href='mainstyle.css'/>
+  </head>
+  <body>
+    <div id='header'>
+      <div id='topbar'>
+        <a href='index.php'>Home</a>
+        <a href='contact.php'>Contact</a>
+        <a href='lmaoayy.html'>Chat</a>
+        <!--
+        <form action='search.php' method='post'>
+          <input type='text' name='query'/>
+          <input type='button' name='search' value='search'/>
+        </form>
+        //-->   
+      </div>
+    </div>
+    <div id='recent'>
+      <h3>Recent posts</h3>
+      <div id='posts'>
+      </div>
+    </div>
+    <div id='footer'>
+      <a href=''>Rules</a>
+    </div>
+  </body>
+</html>
